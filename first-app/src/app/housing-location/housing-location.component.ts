c41e923cd0e207385ecd0521a9d2ad7041e5d517
Mohammad Ajazuddin <mohd.ajazzuddin@gmail.com>
2024-02-28 12:55:43 +0530

    Added Inputs(@input()) of type HousingLocation to HousingComponent

--- first-app/src/app/housing-location/housing-location.component.ts
+++ first-app/src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css']
 })
-export class HousingLocationComponent {
 
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+  //You have to add the ! because the input is expecting the value to be passed. In this case, there is no default value. 
 }
