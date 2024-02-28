5e67bafa01505c3f695f80a65954bc66c73542d6
Mohammad Ajazuddin <mohd.ajazzuddin@gmail.com>
2024-02-28 13:45:44 +0530

    Configured routing

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -6,16 +6,34 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HousingLocationComponent } from './housing-location/housing-location.component';
 import { HousingService } from './housing.service';
+import { DetailsComponent } from './details/details.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes : Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page'
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    title: 'Home details'
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HousingLocationComponent
+    HousingLocationComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [HousingService],
   bootstrap: [AppComponent]
