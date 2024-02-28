124d8b8792383c4973b9b04cf0809fb1855c5f5c
Mohammad Ajazuddin <mohd.ajazzuddin@gmail.com>
2024-02-28 14:49:18 +0530

    route param to retrieve data from HousingService to display specific housing location info

--- first-app/src/app/details/details.component.ts
+++ first-app/src/app/details/details.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { HousingService } from '../housing.service';
+import { ActivatedRoute } from '@angular/router';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
 
+
+HousingLocationComponent
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+  
+  housingLocationId = -1
+
+  housingLocation:HousingLocation|undefined;
+
+  constructor(private currentRoute:ActivatedRoute, private housingService:HousingService)
+  {
+    this.housingLocationId = Number(this.currentRoute.snapshot.params['id']);
+
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+  }
+
+
 
 }
