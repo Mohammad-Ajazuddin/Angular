bbe10c0c8a3ac56bbee6e9468f3e47d347999108
Mohammad Ajazuddin <mohd.ajazzuddin@gmail.com>
2024-02-28 15:29:01 +0530

    template ref var, event binding & search functionality

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -11,11 +11,23 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
 
   housingLocationList: HousingLocation[] = [];  
+  filteredLocationList: HousingLocation[] = [];
+
   //dependency injection of service
   constructor(private housingService : HousingService){
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
 
-
-
+  filterResults(inputValue:string)
+  {
+    if (!inputValue) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+  
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  }
 }
