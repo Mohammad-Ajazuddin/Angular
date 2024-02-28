7c952011ee4a256102b7631d80e958b5ad8895aa
Mohammad Ajazuddin <mohd.ajazzuddin@gmail.com>
2024-02-28 12:48:57 +0530

    Created Home and HousingLocation Components & HousingLocation Interface

---
+++ first-app/src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent {
+
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+
+}
